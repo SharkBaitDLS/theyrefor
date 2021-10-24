bbdb06c356686b63f5ee6a45888f25b18a26c548
Isaac Good <imgood@imgoodproductions.org>
2021-10-23 17:10:48 -0700

    Update redirect in HTTP guard

--- ui/src/http_client.rs
+++ ui/src/http_client.rs
@@ -1,28 +1,52 @@
 use anyhow::anyhow;
 use log::error;
-use reqwasm::{http::Request, Error};
+use reqwasm::{
+   http::{Request, Response},
+   Error,
+};
 use serde::de::DeserializeOwned;
+use theyrefor_models::AuthState;
+use url::Url;
 use yew::services::fetch::StatusCode;
+
+static STATE_PARAM: &str = "state";
 
 pub async fn get<T>(uri: &str) -> Result<Option<T>, Error>
 where
    T: DeserializeOwned,
 {
    match Request::get(uri).send().await {
-      Ok(response) if response.status() == StatusCode::UNAUTHORIZED => {
-         // TODO: extract token and rewrite redirect location in state
-         let base_redirect = response.text().await.unwrap();
-         let location = web_sys::window().unwrap().location();
-
-         location
-            .set_href(&base_redirect)
-            .map(|_| None)
-            .map_err(|_| Error::Other(anyhow!("")))
-      }
+      Ok(response) if response.status() == StatusCode::UNAUTHORIZED => update_redirect(response).await,
       Ok(response) => response.json().await,
       Err(err) => {
          error!("{:?}", err);
          Err(err)
       }
    }
+}
+
+async fn update_redirect<T>(response: Response) -> Result<Option<T>, Error> {
+   let location = web_sys::window().unwrap().location();
+
+   let url = response
+      .text()
+      .await
+      .ok()
+      .and_then(|text| Url::parse(&text).ok())
+      .unwrap();
+   let state_param = url.query_pairs().find(|pair| pair.0 == STATE_PARAM).unwrap().1;
+   let mut auth_state: AuthState = bincode::deserialize(&base64::decode(&*state_param).unwrap()).unwrap();
+   auth_state.redirect_to = location.href().ok();
+
+   let remainder = url.query_pairs().filter(|pair| pair.0 != STATE_PARAM);
+   let mut url = url.clone();
+   url.set_query(None);
+   url.query_pairs_mut()
+      .extend_pairs(remainder)
+      .append_pair(STATE_PARAM, &base64::encode(&bincode::serialize(&auth_state).unwrap()));
+
+   location
+      .set_href(url.as_str())
+      .map(|_| None)
+      .map_err(|_| Error::Other(anyhow!("Failed to update HREF")))
 }
