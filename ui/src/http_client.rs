f519a512c92299d07c2e0e303f98ce11dc32426c
Isaac M. Good <imgood@imgoodproductions.org>
2022-02-20 14:59:40 -0800

    Additional debug logging around auth issues

--- ui/src/http_client.rs
+++ ui/src/http_client.rs
@@ -76,12 +76,14 @@ pub async fn delete_with_auth(uri: &str) -> Result<Option<()>, ClientError> {
 async fn update_redirect<T>(response: Response) -> Result<Option<T>, ClientError> {
    let location = web_sys::window().unwrap().location();
 
-   let url = response
-      .text()
-      .await
-      .ok()
-      .and_then(|text| Url::parse(&text).ok())
-      .unwrap();
+   let text = response.text().await;
+   let maybe_url = text.as_ref().ok().and_then(|text| Url::parse(text).ok());
+   if maybe_url.is_none() {
+      error!("Could not parse redirect uri: {}", text.unwrap_or_default());
+      return Ok(None);
+   }
+   let url = maybe_url.unwrap();
+
    let state_param = url.query_pairs().find(|pair| pair.0 == STATE_PARAM).unwrap().1;
    let mut auth_state: AuthState = bincode::deserialize(&base64::decode(&*state_param).unwrap()).unwrap();
    auth_state.redirect_to = location.href().ok();
